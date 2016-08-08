38477370d2528a4d251192a671aa1a0816e63ef3
yvkschaefer <yvkschaefer@gmail.com>
2016-08-08 19:31:37 +0000

    node modules installed, site is up all tests look good, ready to start first exercise

---
+++ src/js/components/App.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <main>
+        <h1>My first React App</h1>
+        <hr/>
+      </main>
+    );
+  }
+});
+
+module.exports = App;
